991654a66e90b98ed69078880d3e47ce23ce3538
Pabsyyy <christiangalinato1218@gmail.com>
2024-09-15 15:35:28 +0800

    initial commit

---
+++ dataConversion.js
@@ -0,0 +1,28 @@
+//STRING TO NUMBER
+let price = "200";
+
+let intPrice = parseInt(price);
+console.log("parseInt:", intPrice); 
+
+let numPrice = Number(price);
+console.log("Number:", numPrice); 
+
+let plusPrice = +price;
+console.log("+operator:", plusPrice); 
+
+
+//NUMBER TO STRING
+let distance = 150;
+
+let stringDistance1 = distance.toString();
+console.log(".toString():", stringDistance1); 
+
+let stringDistance2 = String(distance);
+console.log("String():", stringDistance2); 
+
+
+//STRING TO DECIMAL NUMBER
+let weight = "72.5";
+
+decimalWeight = parseFloat(weight);
+console.log("parseFloat():", decimalWeight); 
